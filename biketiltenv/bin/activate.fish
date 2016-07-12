72e42d8415da3e099ff2bd09d8d4f5a9eb6a47db
Marek Syldatk <marek@syldatk.com>
2016-07-12 21:47:16 +0200

    Remove virtualenv folder.

--- biketiltenv/bin/activate.fish
+++ biketiltenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/topmarek/Documents/git/bike_tilt/biketiltenv"
+set -gx VIRTUAL_ENV "/Users/marek/Documents/git/biketilt/biketiltenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
