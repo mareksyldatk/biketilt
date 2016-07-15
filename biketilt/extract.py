9ae0105846488148e307edfde93ea0ac6dbe0703
Marek Syldatk <marek@syldatk.com>
2016-07-15 15:11:22 +0100

    Now extracting all highways. Replace tar.gz with 7z

--- biketilt/extract.py
+++ biketilt/extract.py
@@ -2,6 +2,7 @@ from imposm.parser import OSMParser
 import pandas
 import pickle
 
+
 # Simple class that handles the parsed OSM data.
 class Callbacks(object):
     coords = list()
@@ -11,8 +12,7 @@ class Callbacks(object):
         # callback method for ways
         for osmid, tags, refs in ways:
             if 'highway' in tags:
-                if 'trunk' in tags.values():
-                    self.ways.append((osmid, refs))
+                self.ways.append((osmid, tags, refs))
 
     def coords_callback(self, coords):
         for osmid, lon, lat in coords:
@@ -21,19 +21,16 @@ class Callbacks(object):
 
 # Instantiate parser and start parsing
 callbacks = Callbacks()
-
-p1 = OSMParser(concurrency=4, coords_callback=callbacks.coords_callback)
-p1.parse('data/zachodniopomorskie-latest.osm.pbf')
-
-p2 = OSMParser(concurrency=4, ways_callback=callbacks.ways_callback)
-p2.parse('data/zachodniopomorskie-latest.osm.pbf')
-
-# Extracted data
+p = OSMParser(concurrency=4, coords_callback=callbacks.coords_callback)
+p.parse('data/zachodniopomorskie-latest.osm.pbf')
 data_coords = pandas.DataFrame(callbacks.coords)
 data_coords.columns = ['id', 'lon', 'lat']
 
+callbacks = Callbacks()
+p = OSMParser(concurrency=4, ways_callback=callbacks.ways_callback)
+p.parse('data/zachodniopomorskie-latest.osm.pbf')
 data_ways = pandas.DataFrame(callbacks.ways)
-data_ways.columns = ['id', 'refs']
+data_ways.columns = ['id', 'tags', 'refs']
 
 # Dump data
 with open('extracted/zachodniopomorskie_trunk.pickle', 'wb') as handle:
--- biketilt/process.py
+++ biketilt/process.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 with open('extracted/zachodniopomorskie_trunk.pickle', 'rb') as handle:
     [ways, points] = pickle.load(handle)
 
-for i in range(0, len(ways)):
+for i in range(0, 100):
     a_way = ways.iloc[[i]]
     way_points = list(a_way['refs'])[0]
     way_coords = points.loc[points['id'].isin(way_points)]
-    way_lon = list(way_coords['lon'])
     way_lat = list(way_coords['lat'])
+    way_lon = list(way_coords['lon'])
 
     plt.plot(way_lat, way_lon, '.')
 
