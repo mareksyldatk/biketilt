f644ec83d422aeff9abccdb20e5a1fe74bb64589
Marek Syldatk <marek@syldatk.com>
2016-07-17 20:19:22 +0200

    Analysis counted

--- biketilt/trips/trip_analysis.R
+++ biketilt/trips/trip_analysis.R
@@ -7,4 +7,6 @@ d3 <- read.csv("20160716200736.csv")
 df <- rbind.data.frame(d1, d2, d3)
 df <- df[complete.cases(df),]
 
+df %>% write.csv(.,file = "df_20160716123101.csv")
+
 qplot(Longitude, Latitude, data=df, colour=Velocity) + scale_colour_gradient(low="white", high="blue") + coord_fixed(ratio = 1)
