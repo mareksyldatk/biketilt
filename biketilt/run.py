2750af31ed076eb32ad0f5f33bae098e6d61717e
Marek Syldatk <marek@syldatk.com>
2016-12-13 21:15:56 +0100

    Extra stuff

--- biketilt/run.py
+++ biketilt/run.py
@@ -1,21 +1,108 @@
+# -*- coding: UTF-8 -*-
+
 import matplotlib.pyplot as plt
 import pandas
 import tables
 import numpy
 from pylab import *
+import unicodedata
+from trans import trans
 
+from unidecode import unidecode
+
+def transform(x):
+    if x is not None:
+       return trans(x)
+
+def export_to_csv(data_frame, output_file):
+    data_frame = data_frame.drop('order', 1)
+    data_frame = data_frame.rename(columns={'lon': 'latitude', 'lat': 'longitude'})
+    data_frame['name'] = data_frame['name'].apply(transform)
+    data_frame[[u'id', u'name', u'latitude', u'longitude', u'point_id', u'type', ]].to_csv(output_file, encoding='utf-8')
 
 # Load data
-roads = pandas.read_hdf("data/zachodniopomorskie.h5", key="roads")
+roads = pandas.read_hdf("data/pomorskie.h5", key="roads")
+
+# Dump data to csv
+# export_to_csv(roads, "csv/zachodniopomorskie.csv")
+
+# Plot
+# desired_road_types = ['motorway', 'motorway_link', 'trunk', 'primary', 'primary_link', 'secondary', 'tertiary', 'unclassified', 'road', 'residential']
+#
+# clr = map(str, numpy.linspace(0.9, 0.1, len(desired_road_types)))
+# siz = numpy.linspace(2, .5, len(desired_road_types))
+#
+# for ix, road_type in enumerate(reversed(desired_road_types)):
+#     plot_road = roads[roads['type'] == road_type]
+#     plt.scatter(plot_road['lon'], plot_road['lat'], c=clr[ix], s=siz[ix], edgecolors='none')
+#
+# plt.title("Zachodniopomorskie")
+
+
+def circle_radius(p1x, p1y, p2x, p2y, p3x, p3y):
+    try:
+        x1 = (p2x + p1x) / 2.
+        y11 = (p2y + p1y) / 2.
+        dy1 = p2x - p1x
+        dx1 = -(p2y - p1y)
+
+        x2 = (p3x + p2x) / 2.
+        y2 = (p3y + p2y) / 2.
+        dy2 = p3x - p2x
+        dx2 = -(p3y - p2y)
+
+        ox = (y11 * dx1 * dx2 + x2 * dx1 * dy2 - x1 * dy1 * dx2 - y2 * dx1 * dx2)/ (dx1 * dy2 - dy1 * dx2)
+        oy = (ox - x1) * dy1 / dx1 + y11
+
+        dx = ox - p1x
+        dy = oy - p1y
+
+        return np.sqrt(dx * dx + dy * dy)
+    except:
+        return None
+
+def compute_radius(row):
+    p1x, p1y = row['p_lat'], row['p_lon']
+    p2x, p2y = row['lat'], row['lon']
+    p3x, p3y = row['n_lat'], row['n_lon']
+
+    if sum(isnan([ p1x, p1y, p2x, p2y, p3x, p3y ])) == 0:
+        return circle_radius(p1x, p1y, p2x, p2y, p3x, p3y)
+    else:
+        return None
+
+
+df = roads.sort_values(by=['id', 'order'])
+
+df['p_lon'] = df.groupby(['id'])['lon'].shift(1)
+df['p_lat'] = df.groupby(['id'])['lat'].shift(1)
+df['n_lon'] = df.groupby(['id'])['lon'].shift(-1)
+df['n_lat'] = df.groupby(['id'])['lat'].shift(-1)
+df['radius'] = radius = df.apply(compute_radius, 1)
+df = df.drop(['p_lat', 'p_lon', 'n_lat', 'n_lon'], 1)
+
+df['mean_radius'] = df[['id','radius']].groupby(['id']).transform(mean)
+
+
 
 # Plot
 desired_road_types = ['motorway', 'motorway_link', 'trunk', 'primary', 'primary_link', 'secondary', 'tertiary', 'unclassified', 'road', 'residential']
 
-clr = map(str, numpy.linspace(0.9, 0.1, len(desired_road_types)))
-siz = numpy.linspace(2, .5, len(desired_road_types))
 
-for ix, road_type in enumerate(reversed(desired_road_types)):
-    plot_road = roads[roads['type'] == road_type]
-    plt.scatter(plot_road['lon'], plot_road['lat'], c=clr[ix], s=siz[ix], edgecolors='none')
+data = df[df['type'].isin(desired_road_types)]
+data = data[data['mean_radius'].notnull()]
 
-plt.title("Zachodniopomorskie")
+data['mean_radius'] = np.log(data['mean_radius'])
+data['mean_radius'] *= -1.
+data['mean_radius'] = (data['mean_radius'] - data['mean_radius'].min()) / (data['mean_radius'].max() - data['mean_radius'].min())
+
+
+fig, ax = plt.subplots()
+data.plot(x='lon', y='lat', kind='scatter', c='mean_radius', cmap='hot', edgecolors='none', s=.001, ax=ax)
+ax.set_axis_bgcolor('black')
+plt.axis('equal')
+
+fig.savefig('myimage.png', format='png', dpi=2400, bbox_inches='tight', pad_inches=0)
+
+
+
--- biketilt/trips/trip_analysis.R
+++ biketilt/trips/trip_analysis.R
@@ -1,12 +1,66 @@
 library(dplyr)
 library(ggplot2)
+library(RgoogleMaps)
+library(png)
+library(grid)
+library(rgdal)
 
-d1 <- read.csv("20160716123101.csv")
-d2 <- read.csv("20160716163834.csv")
-d3 <- read.csv("20160716200736.csv")
-df <- rbind.data.frame(d1, d2, d3)
+preprocess.csvA <- function(filename) {
+  head(read.csv(filename), -6) %>% mutate(Speed=Velocity) %>% select(Latitude, Longitude, Speed)
+}
+preprocess.csvB <- function(filename) {
+  read.csv(filename) %>% mutate(Speed = Speed.m.s. * 3.6) %>% select(Latitude, Longitude, Speed)
+}
+preprocess.kml <- function(filename, layername) {
+  coordinates(readOGR(dsn=filename, layer=layername)) %>% data.frame %>% 
+    rename(Latitude=X2, Longitude=X1) %>% mutate(Speed=66.8827)
+}
+
+# Load data
+t01 <- preprocess.csvA("data/20160711181706.csv")
+t02 <- preprocess.csvA("data/20160711193353.csv")
+t03 <- preprocess.csvA("data/20160715194016.csv")
+t04 <- preprocess.csvA("data/20160716123101.csv")
+t05 <- preprocess.csvA("data/20160716163834.csv")
+t06 <- preprocess.csvA("data/20160716200736.csv")
+t07 <- preprocess.csvA("data/20160718175539.csv")
+t08 <- preprocess.csvA("data/20160721194729.csv")
+t09 <- preprocess.csvA("data/20160724120814.csv")
+t10 <- preprocess.csvA("data/20160724174918.csv")
+
+t11 <- preprocess.kml("data/20160729000000.kml", "Kawno-Miedzyzdroje")
+
+t13 <- preprocess.csvB("data/20160730_Polnocne_Niemcy.csv")
+t14 <- preprocess.csvB("data/20160812_Lazy.csv")
+t15 <- preprocess.csvB("data/20160813_Ustka.csv")
+t16 <- preprocess.csvB("data/20160815_Bobiecino.csv")
+t17 <- preprocess.csvB("data/20160816_Wierzchowo.csv")
+
+# Combine and clean data
+df <- rbind.data.frame(t01, t02, t03, t04, t05, t06, t07, t08, t09, t10, t11, t13, t14, t15, t16, t17)
 df <- df[complete.cases(df),]
+df$Average.Speed <- SMA(df$Speed, 50)
 
-df %>% write.csv(.,file = "df_20160716123101.csv")
+# 
+# Plot
+#
 
-qplot(Longitude, Latitude, data=df, colour=Velocity) + scale_colour_gradient(low="white", high="blue") + coord_fixed(ratio = 1)
+# loading the required packages
+library(ggplot2)
+library(ggmap)
+
+# creating a sample data.frame with your lat/lon points
+lon <- c(13.0, 18.0)
+lat <- c(53.5, 54.5)
+
+# getting the map
+mapgilbert <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 7, maptype = "toner-lite", scale = 1)
+
+# plotting the map with some points on it
+ggmap(mapgilbert, xlim=lon, ylim=lat) +
+  geom_point(data = df, aes(x = Longitude, y = Latitude, col=Average.Speed), size = I(1.0), shape = 21, alpha=0.9) +
+  # scale_colour_gradientn(colours = topo.colors(10)) +
+  scale_colour_gradient(low = "orange", high = "blue") +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE) +
+  labs(x="Longitude", y="Latitude") +
+  ggtitle("Moto Summer 2016 #latoFazera ")
